a5c2b354098bcff63fb264b8e239ac067532a6cb
Ray Kiddy <ray@ganymede.org>
2017-06-07 15:19:05 -0700

    Add page for viewing/editing a Relation. Adds a check if desired.

---
+++ Sources/org/ganymede/boundaries/ui/RelationDetail.java
@@ -0,0 +1,33 @@
+package org.ganymede.boundaries.ui;
+
+import org.ganymede.boundaries.eo.OsmRelation;
+import org.ganymede.boundaries.eo.OsmRelationCheck;
+
+import com.webobjects.appserver.WOActionResults;
+import com.webobjects.appserver.WOContext;
+import com.webobjects.foundation.NSTimestamp;
+
+public class RelationDetail extends BaseComponent {
+
+	private static final long serialVersionUID = 6195636158903218987L;
+
+	public RelationDetail(WOContext context) {
+        super(context);
+    }
+
+	public OsmRelation relation;
+	public OsmRelationCheck check;
+
+	public boolean checkIsGood;
+
+	public WOActionResults addCheck() {
+
+		NSTimestamp now  = new NSTimestamp();
+
+		OsmRelationCheck.createOsmRelationCheck(ec(), checkIsGood ? 1L : 0L, now, now, relation);
+
+		ec().saveChanges();
+
+		return pageWithName(Main.class);
+	}
+}
